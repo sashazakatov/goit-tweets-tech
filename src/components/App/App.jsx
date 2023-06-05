facc2b40e54ab36003f111477cf4699b1b4554a2
Olexander Zakatov <sashazakatov17@gmail.com>
2023-06-05 21:36:04 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home/>}/>
-        <Route path="tweets" element={<Tweets />}/>
+        <Route path="/tweets" element={<Tweets />}/>
       </Route>
       <Route path="*" element={<Navigate to="/" replace />}/>
     </Routes>
